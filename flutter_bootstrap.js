412b1d7d3c6277c2c1f9f9d0e1b174f39694f60c
vbvsone <vbvsoneprojects@gmail.com>
2026-01-16 11:49:11 -0300

    Add messaging panel in profile - Flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1648382874"
+    serviceWorkerVersion: "2236433879"
   }
 });
